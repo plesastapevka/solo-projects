518a70c670503ad18cca7f3bef01fa88610740de
Urban Vidovič <urbanfoundit@gmail.com>
2021-02-19 22:36:09 +0100

    Implemented Edmonds-Karp

--- c_cpp_projects/edmonds_karp/main.cpp
+++ c_cpp_projects/edmonds_karp/main.cpp
@@ -11,7 +11,9 @@ enum STATUS {
 };
 
 struct edge_t {
-    size_t v1, v2, capacity;
+    size_t v1, v2;
+    int capacity;
+    int fuv, fvu;
 };
 
 struct vertice_t {
@@ -20,59 +22,97 @@ struct vertice_t {
     STATUS status;
 };
 
-bool operator==(const vertice_t& v1, const vertice_t& v2) {
-    return (v1.prev == v2.prev && v1.len == v2.len && v1.status == v2.status && v1.index == v2.index);
-}
-
 struct graph_t {
-    graph_t(std::vector<vertice_t> vertices, std::vector<edge_t> edges, std::vector<std::vector<size_t>> adjacency):
+    graph_t(std::vector<vertice_t> vertices, std::vector<edge_t> edges):
         m_vertices(vertices),
-        m_edges(edges),
-        m_adjacency(adjacency){}
+        m_edges(edges){}
 
     std::vector<vertice_t> m_vertices;
     std::vector<edge_t> m_edges;
-    std::vector<std::vector<size_t>> m_adjacency;
 };
 
 // prototypes
-size_t edmonds_karp(graph_t& G, size_t s, size_t t);
-void bfs(graph_t& G, size_t s);
+size_t edmonds_karp(std::shared_ptr<graph_t> G, size_t s, size_t t);
+int get_path(std::shared_ptr<graph_t> G, size_t prev, size_t index);
+void set_f(std::shared_ptr<graph_t> G, size_t prev, size_t index, int path);
+bool bfs(std::shared_ptr<graph_t> G, size_t s, size_t d);
 std::shared_ptr<graph_t> init_graph(const std::string& path);
 
-size_t edmonds_karp(graph_t& G, size_t s, size_t t) {
+size_t edmonds_karp(std::shared_ptr<graph_t> G, size_t s, size_t t) {
     size_t max_flow = 0;
-    for (auto e : G.m_edges) {
+    while (bfs(G, s, t)) {
+        graph_t tmp = *G;
+        vertice_t v = G->m_vertices[t];
+        int path = get_path(G, v.prev, v.index);
+        while (v.index != s) {
+            int new_path = get_path(G, v.prev, v.index);
+            if (new_path < path) path = new_path;
+            v = G->m_vertices[v.prev];
+        }
 
+        v = G->m_vertices[t];
+        while (v.index != s) {
+            set_f(G, v.prev, v.index, path);
+            v = G->m_vertices[v.prev];
+        }
+        max_flow += path;
+    }
+    std::cout << "Max flow: " << max_flow << "\n";
+}
+
+int get_path(std::shared_ptr<graph_t> G, size_t prev, size_t index) {
+    if (prev == -1) return std::numeric_limits<int>::max();
+    for (auto const& e : G->m_edges) {
+        if (e.v1 == G->m_vertices[prev].index && e.v2 == G->m_vertices[index].index) {
+            return e.capacity;
+        }
+    }
+    return std::numeric_limits<int>::max();
+}
+
+void set_f(std::shared_ptr<graph_t> G, size_t prev, size_t index, int path) {
+    if (prev == -1) return;
+    for (auto& e : G->m_edges) {
+        if (e.v1 == G->m_vertices[prev].index && e.v2 == G->m_vertices[index].index) {
+            e.fuv += path;
+            e.fvu -= path;
+        }
     }
 }
 
-void bfs(std::shared_ptr<graph_t> G, size_t s, size_t d) {
+bool bfs(std::shared_ptr<graph_t> G, size_t s, size_t d) {
     std::queue<vertice_t> queue;
-    for (auto v : G->m_vertices) {
+    for (auto& v : G->m_vertices) {
         if (v.index == s) {
             v.status = IN_PROGRESS;
             v.len = 0;
             queue.push(v);
-            break;
+        } else {
+            v.status = NA;
+            v.len = std::numeric_limits<int>::max();
+            v.prev = -1;
         }
     }
+    graph_t tmp = *G;
     vertice_t v{};
     while (!queue.empty()) {
         v = queue.front();
         queue.pop();
-        if (v == G->m_vertices[d]) return;
-        for (auto i : G->m_adjacency[v.index]) {
-            auto u = G->m_vertices[i];
-            if (u.status == NA) {
-                u.status = IN_PROGRESS;
-                u.len = v.len + 1;
-                u.prev = v.index;
-                queue.push(u);
+        if (v.index == G->m_vertices[d].index) return true;
+        for (auto& link : G->m_edges) { // iterate through v's neighbors
+            if (link.v1 == v.index) {
+                int neighbor = link.v2;
+                if (G->m_vertices[neighbor].status == NA && (link.capacity - link.fuv) > 0) {
+                    G->m_vertices[neighbor].status = IN_PROGRESS;
+                    G->m_vertices[neighbor].len = v.len + link.capacity;
+                    G->m_vertices[neighbor].prev = v.index;
+                    queue.push(G->m_vertices[neighbor]);
+                }
             }
         }
-        v.status = DEVELOPED;
+        G->m_vertices[v.index].status = DEVELOPED;
     }
+    return false;
 }
 
 std::shared_ptr<graph_t> init_graph(const std::string& path) {
@@ -85,7 +125,6 @@ std::shared_ptr<graph_t> init_graph(const std::string& path) {
     std::vector<edge_t> edges;
     std::string line, val;
     std::vector<size_t> vals;
-    std::vector<std::vector<size_t>> adjacency;
     while (std::getline(file, line)) {
         std::stringstream ss(line);
         while (getline(ss, val, ' ')) {
@@ -100,21 +139,20 @@ std::shared_ptr<graph_t> init_graph(const std::string& path) {
                 v.index = i;
                 vertices.push_back(v);
             }
-            adjacency.resize(vals[0]);
             vals.clear();
         } else {
             edge_t edge{};
             edge.v1 = vals[0];
             edge.v2 = vals[1];
             edge.capacity = vals[2];
+            edge.fuv = 0;
+            edge.fvu = 0;
             edges.push_back(edge_t(edge));
-            adjacency[edge.v1].push_back(edge.v2);
-            adjacency[edge.v2].push_back(edge.v1);
             vals.clear();
         }
     }
     file.close();
-    graph_t graph (vertices, edges, adjacency);
+    graph_t graph (vertices, edges);
     return std::make_shared<graph_t>(graph);
 }
 
@@ -123,5 +161,7 @@ int main() {
     if (graph == nullptr) {
         return 1;
     }
+    size_t start = 0, end = 5;
+    edmonds_karp(graph, start, end);
     return 0;
 }
