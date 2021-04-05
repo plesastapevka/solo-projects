a71dbadd7d17a5524108f22fbb58be7e4ab547f6
Urban Vidovič <urbanfoundit@gmail.com>
2021-04-05 21:01:54 +0200

    Add models and finish n-grams language detection

--- python_projects/n_grams/main.py
+++ python_projects/n_grams/main.py
@@ -1,10 +1,13 @@
 import collections
 import re
+import lxml.etree as ET
+import json
+from lxml import objectify
 
 
-def pre_process_text(path_name):
-    text = re.sub('<[^<]+>', "", open(path_name).read())
-    raw_text = re.sub(r'[^A-Za-zČčŠšŽžĐđäöüßÄÖÜẞ. ]', '', text)
+def pre_process_text(text):
+    clean_text = re.sub('<[^<]+>', "", text)
+    raw_text = re.sub(r'[^A-Za-zČčŠšŽžĐđäöüßÄÖÜẞ. ]', '', clean_text)
     return raw_text.lower()
 
 
@@ -15,35 +18,113 @@ def create_n_grams(text, n=3):
     return [i[0] for i in ngrams_map]
 
 
-def get_ngrams(path_name):
-    text = pre_process_text(path_name)
-    bigrams = create_n_grams(text, 2)
-    trigrams = create_n_grams(text, 3)
-    fourgrams = create_n_grams(text, 4)
-    fivegrams = create_n_grams(text, 5)
+def get_ngrams(path_name="", string=False, input_text=""):
+    if not string:
+        text = open(path_name).read()
+        raw_text = pre_process_text(text)
+        bigrams = create_n_grams(raw_text, 2)
+        trigrams = create_n_grams(raw_text, 3)
+        fourgrams = create_n_grams(raw_text, 4)
+        fivegrams = create_n_grams(raw_text, 5)
+    else:
+        raw_text = pre_process_text(input_text)
+        bigrams = create_n_grams(raw_text, 2)
+        trigrams = create_n_grams(raw_text, 3)
+        fourgrams = create_n_grams(raw_text, 4)
+        fivegrams = create_n_grams(raw_text, 5)
+
     return bigrams, trigrams, fourgrams, fivegrams
 
 
+def get_diff(ngram, element, idx):
+    try:
+        index = ngram.index(element)
+        return abs(index - idx)
+    except ValueError:
+        return len(ngram)
+
+
 def detect_lang(bi, tri, four, five):
-    # TODO: create ngrams for eng, ger and si lang
-    bi_eng = ["e "]
-    tri_eng = []
-    four_eng = []
-    five_eng = []
-    sim_sum = 0
-    for idx, bi_e in enumerate(bi):
-        try:
-            index = bi_eng.index(bi_e)
-            sim_sum += abs(index - idx)
-        except ValueError:
-            pass
-    print(sim_sum)
+    bi_si, tri_si, four_si, five_si = get_ngrams("models/si.txt")
+    bi_eng, tri_eng, four_eng, five_eng = get_ngrams("models/eng.txt")
+    bi_ger, tri_ger, four_ger, five_ger = get_ngrams("models/ger.txt")
+    sim_sum_si = 0
+    sim_sum_eng = 0
+    sim_sum_ger = 0
+
+    # bigram processing
+    for idx, e in enumerate(bi):
+        diff_si = get_diff(bi_si, e, idx)
+        diff_eng = get_diff(bi_eng, e, idx)
+        diff_ger = get_diff(bi_ger, e, idx)
+        if diff_si != -1:
+            sim_sum_si += diff_si
+        if diff_eng != -1:
+            sim_sum_eng += diff_eng
+        if diff_ger != -1:
+            sim_sum_ger += diff_ger
+
+    # trigram processing
+    for idx, e in enumerate(tri):
+        diff_si = get_diff(tri_si, e, idx)
+        diff_eng = get_diff(tri_eng, e, idx)
+        diff_ger = get_diff(tri_ger, e, idx)
+        if diff_si != -1:
+            sim_sum_si += diff_si
+        if diff_eng != -1:
+            sim_sum_eng += diff_eng
+        if diff_ger != -1:
+            sim_sum_ger += diff_ger
+
+    # fourgram processing
+    for idx, e in enumerate(four):
+        diff_si = get_diff(four_si, e, idx)
+        diff_eng = get_diff(four_eng, e, idx)
+        diff_ger = get_diff(four_ger, e, idx)
+        if diff_si != -1:
+            sim_sum_si += diff_si
+        if diff_eng != -1:
+            sim_sum_eng += diff_eng
+        if diff_ger != -1:
+            sim_sum_ger += diff_ger
+
+    # fivegram processing
+    # for idx, e in enumerate(five):
+    #     diff_si = get_diff(five_si, e, idx)
+    #     diff_eng = get_diff(five_eng, e, idx)
+    #     diff_ger = get_diff(five_ger, e, idx)
+    #     if diff_si != -1:
+    #         sim_sum_si += diff_si
+    #     if diff_eng != -1:
+    #         sim_sum_eng += diff_eng
+    #     if diff_ger != -1:
+    #         sim_sum_ger += diff_ger
+
+    # print("English similarity sum is: " + str(sim_sum_eng))
+    # print("Slovene similarity sum is: " + str(sim_sum_si))
+    # print("German similarity sum is: " + str(sim_sum_ger))
+    if min([sim_sum_si, sim_sum_eng, sim_sum_ger]) == sim_sum_eng:
+        return "english"
+    elif min([sim_sum_si, sim_sum_eng, sim_sum_ger]) == sim_sum_si:
+        return "slovenian"
+    elif min([sim_sum_si, sim_sum_eng, sim_sum_ger]) == sim_sum_ger:
+        return "german"
 
 
-def main():
-    bigrams, trigrams, fourgrams, fivegrams = get_ngrams("korpus/kas-4000.text.xml")
-    print(list(bigrams))
-    detect_lang(bigrams, trigrams, fourgrams, fivegrams)
+def main(flag=False):
+    if flag:
+        bigrams, trigrams, fourgrams, fivegrams = get_ngrams("korpus/kas-5000.text.xml")
+        print("Detecting language ...")
+        detect_lang(bigrams, trigrams, fourgrams, fivegrams)
+    else:
+        xml_tree = ET.parse("korpus/kas-5000.text.xml")
+        paragraphs = xml_tree.findall("page/p")
+
+        for p in paragraphs:
+            bigrams, trigrams, fourgrams, fivegrams = get_ngrams(input_text=p.text, string=True)
+            lang = detect_lang(bigrams, trigrams, fourgrams, fivegrams)
+            id = p.attrib.values()[0]
+            print(id + ": " + lang)
 
 
 if __name__ == "__main__":
