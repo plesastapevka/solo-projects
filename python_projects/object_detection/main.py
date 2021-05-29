15ed75799a074bca6b462059b2643102ae203916
Urban Vidovič <urbanfoundit@gmail.com>
2021-05-29 20:17:28 +0200

    Remove unnecessary code

--- python_projects/object_detection/main.py
+++ python_projects/object_detection/main.py
@@ -10,21 +10,18 @@ from object_detection.utils import visualization_utils as vis_util
 PATH_TO_LABELS = 'object_detection/data/mscoco_label_map.pbtxt'
 MODEL_NAME = 'ssd_inception_v2_coco_2017_11_17'
 
-while "models" in pathlib.Path.cwd().parts:
-    os.chdir('..')
-
 
 def load_model(model_name):
     url = 'http://download.tensorflow.org/models/object_detection/'
-    model_file = model_name + '.tar.gz'
-    model_dir = tf.keras.utils.get_file(
+    file = model_name + '.tar.gz'
+    directory = tf.keras.utils.get_file(
         fname=model_name,
-        origin=url + model_file,
+        origin=url + file,
         untar=True)
 
-    model_dir = pathlib.Path(model_dir) / "saved_model"
+    directory = pathlib.Path(directory) / "saved_model"
 
-    model = tf.saved_model.load(str(model_dir))
+    model = tf.saved_model.load(str(directory))
 
     return model
 
@@ -35,35 +32,21 @@ DETECTION_MODEL = load_model(MODEL_NAME)
 
 def run_inference(model, image):
     image = np.asarray(image)
-    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
     input_tensor = tf.convert_to_tensor(image)
-    # The model expects a batch of images, so add an axis with `tf.newaxis`.
     input_tensor = input_tensor[tf.newaxis, ...]
 
     # Run inference
     model_fn = model.signatures['serving_default']
     output_dict = model_fn(input_tensor)
 
-    # All outputs are batches tensors.
-    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
-    # We're only interested in the first num_detections.
+    # We only need first num_detections
     num_detections = int(output_dict.pop('num_detections'))
     output_dict = {key: value[0, :num_detections].numpy()
                    for key, value in output_dict.items()}
     output_dict['num_detections'] = num_detections
 
-    # detection_classes should be ints.
+    # Convert detection_classes to ints
     output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-
-    # Handle models with masks:
-    if 'detection_masks' in output_dict:
-        # Reframe the the bbox mask to the image size.
-        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-            output_dict['detection_masks'], output_dict['detection_boxes'],
-            image.shape[0], image.shape[1])
-        detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
-                                           tf.uint8)
-        output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
 
     return output_dict
 
