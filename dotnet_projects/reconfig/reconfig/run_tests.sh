7d1487daebbd6f74d80ebdd137318b52844bc066
Urban Vidovič <urbanfoundit@gmail.com>
2021-09-21 23:07:03 +0200

    Add test script for reconfig project

---
+++ dotnet_projects/reconfig/reconfig/run_tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+dotnet run data/G1.txt S s
+dotnet run data/G1.txt I s
+dotnet run data/G1.txt C s
+
+dotnet run data/G2.txt S s
+dotnet run data/G2.txt I s
+dotnet run data/G2.txt C s
+
+dotnet run data/G3.txt S s
+dotnet run data/G3.txt I s
+dotnet run data/G3.txt C s
+
+dotnet run data/G4.txt S s
+dotnet run data/G4.txt I s
+dotnet run data/G4.txt C s
+
+dotnet run data/G5.txt S s
+dotnet run data/G5.txt I s
+dotnet run data/G5.txt C s
