d4ecaddaee0dbd873be1b46ea20da11108d7c143
Urban Vidovic <urbanfoundit@gmail.com>
2021-10-17 23:33:31 +0200

    Should move to more RAM due to big allocation

--- dotnet_projects/sentinel_index/sentinel_index/Program.cs
+++ dotnet_projects/sentinel_index/sentinel_index/Program.cs
@@ -1,28 +1,31 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using Aspose.Imaging;
+using OpenJpegDotNet;
+using Image = Aspose.Imaging.Image;
 
 namespace sentinel_index
 {
     class Program
     {
-        [SuppressMessage("ReSharper.DPA", "DPA0001: Memory allocation issues")]
         static void Main(string[] args)
         {
-            Image image = Image.Load(@"T33TWM_20211009T095029_B01.jp2");
-            RasterImage raster = (RasterImage) image;
+            RasterImage band2 = (RasterImage)Image.Load(@"T33TWM_20211009T095029_B02.jp2");
+            RasterImage band4 = (RasterImage)Image.Load(@"T33TWM_20211009T095029_B04.jp2");
+            RasterImage band8 = (RasterImage)Image.Load(@"T33TWM_20211009T095029_B08.jp2");
+            // Color[] band4Pxls = band4.LoadPixels(band4.Bounds);
+            // Color[] band8Pxls = band8.LoadPixels(band8.Bounds);
+            // Color[] EVI = new Color[band2Pxls.Length];
+            for (int i = 0; i < band2.Height; i++)
+            {
+                Console.WriteLine(band2.GetPixel(i, 0).R);
+                break;
+                // byte R = (band8Pxls[i].R - band4Pxls[i].R)
+                // EVI[i] = new Color();
+            }
 
-            var height = image.Height;
-            var width = image.Width;
-            Console.WriteLine(raster.GetPixel(544, 547));
-            
-            // for (int i = 0; i < height; i++)
-            // {   
-                // Console.WriteLine(raster.GetPixel(i, 0).ToString());
-            // }
-            
-            var options = new Aspose.Imaging.ImageOptions.PngOptions();
-            image.Save(@"output.png", options);
+            // var options = new Aspose.Imaging.ImageOptions.PngOptions();
+            // image.Save(@"output.png", options);
         }
     }
 }
