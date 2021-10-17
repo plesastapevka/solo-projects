80b3d3b8197e1bb09682926ea392f5dd7cd716eb
Urban Vidovic <urbanfoundit@gmail.com>
2021-10-17 15:48:00 +0200

    Add sentinel_index project

---
+++ dotnet_projects/sentinel_index/sentinel_index/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sentinel_index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
