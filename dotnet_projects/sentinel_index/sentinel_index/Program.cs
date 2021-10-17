f64407bb5ed18f07c59be4b35783a1eb97f948f5
Urban Vidovic <urbanfoundit@gmail.com>
2021-10-17 17:56:16 +0200

    Implement .jp2 file reading

--- dotnet_projects/sentinel_index/sentinel_index/Program.cs
+++ dotnet_projects/sentinel_index/sentinel_index/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Aspose.Imaging;
 
 namespace sentinel_index
 {
     class Program
     {
+        [SuppressMessage("ReSharper.DPA", "DPA0001: Memory allocation issues")]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Image image = Image.Load(@"T33TWM_20211009T095029_B01.jp2");
+            RasterImage raster = (RasterImage) image;
+
+            var height = image.Height;
+            var width = image.Width;
+            Console.WriteLine(raster.GetPixel(544, 547));
+            
+            // for (int i = 0; i < height; i++)
+            // {   
+                // Console.WriteLine(raster.GetPixel(i, 0).ToString());
+            // }
+            
+            var options = new Aspose.Imaging.ImageOptions.PngOptions();
+            image.Save(@"output.png", options);
         }
     }
 }
